83d12eed086534b0704e022f2587b12c3d9221c6
wangdefeng <wangdefeng@zbitedu.cn>
2022-06-09 19:55:53 +0800

    订单增删改查

--- bailian-order/src/main/java/com/cuning/BailianOrderApplication.java
+++ bailian-order/src/main/java/com/cuning/BailianOrderApplication.java
@@ -1,10 +1,15 @@
 package com.cuning;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.cuning.mapper")
+@EnableSwagger2
+@EnableSwaggerBootstrapUI
 public class BailianOrderApplication {
 
     public static void main(String[] args) {
