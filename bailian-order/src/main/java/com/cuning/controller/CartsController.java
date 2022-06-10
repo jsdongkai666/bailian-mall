670d42e135c637693073315ba7ec724453595559
dingjunkai <2240299631@qq.com>
2022-06-10 14:28:18 +0800

    购物车的增删改查

--- bailian-order/src/main/java/com/cuning/controller/CartsController.java
+++ bailian-order/src/main/java/com/cuning/controller/CartsController.java
@@ -5,10 +5,10 @@ import com.cuning.service.CartsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Api(tags = "购物车模块")
@@ -21,5 +21,23 @@ public class CartsController {
     @GetMapping("/getCartsList")
     public List<BailianCarts> getCartsList(){
         return cartsService.getCartsList();
+    }
+
+    @ApiOperation(value = "新增购物车信息")
+    @PostMapping("/addCartsList")
+    public boolean addCartsList(@RequestBody BailianCarts bailianCarts){
+        return cartsService.addCarts(bailianCarts);
+    }
+
+    @ApiOperation(value = "删除购物车信息")
+    @PostMapping("/delCartsList")
+    public boolean deleteCartsByBatchIds(@RequestParam List<String> ids){
+        return cartsService.deleteCartsById(ids);
+    }
+
+    @ApiOperation(value = "修改购物车信息")
+    @PostMapping("modCarts")
+    public boolean modifyCarts(@RequestBody BailianCarts bailianCarts){
+        return cartsService.modifyCarts(bailianCarts);
     }
 }
