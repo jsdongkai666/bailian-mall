bc82f2986a819b30128650f778e290d37badcd7e
SakuraBacteria <jinmutianlong@qq.com>
2022-06-11 16:40:25 +0800

    token

--- bailian-user/src/main/java/com/cuning/service/impl/UserServiceImpl.java
+++ bailian-user/src/main/java/com/cuning/service/impl/UserServiceImpl.java
@@ -95,31 +95,45 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         // openid存在 微信用户绑定手机号
         // 为空 单纯手机号登录
 
+        // 把手机和验证码绑定起来
+        redisUtils.set(telCaptcha,tel,60);
+
         if (StringUtils.isEmpty(openid)){
             if (captcha.equalsIgnoreCase(telCaptcha)){
                 // 验证码正确
-                LambdaQueryWrapper<User> qw = new LambdaQueryWrapper<>();
-                // 查询是否存在tel 一个tel对应一个用户
-                qw.eq(User::getUserTel,tel);
-                User one = userMapper.selectOne(qw);
-                // 不存在这个手机 插入用户
-                if (one == null){
-                    User insert = User.builder().userId(Long.toString(snowFlake.nextId())).userTel(tel).build();
-                    int row = userMapper.insert(insert);
-                    // 插入成功
-                    if (row > 0){
-                        log.info("插入成功");
-                        resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,insert);
-                    }else {
-                        log.info("插入失败");
-                        resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"登录失败");
-                    }
+                // 同时验证码与手机号对应才满足条件
+               if (tel.equals(redisUtils.get(telCaptcha))){
+                   LambdaQueryWrapper<User> qw = new LambdaQueryWrapper<>();
+                   // 查询是否存在tel 一个tel对应一个用户
+                   qw.eq(User::getUserTel,tel);
+                   User one = userMapper.selectOne(qw);
+                   // 不存在这个手机 插入用户
+                   if (one == null){
+                       User insert = User.builder().userId(Long.toString(snowFlake.nextId())).userTel(tel).build();
+                       int row = userMapper.insert(insert);
+                       // 插入成功
+                       if (row > 0){
+                           log.info("插入成功");
+                           resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,insert);
+                       }else {
+                           log.info("插入失败");
+                           resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"登录失败");
+                       }
 
-                }
-                // 存在直接返回
+                   }
+                   // 存在直接返回
 
-                resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,one);
+                  else{
+                       resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,one);
+                   }
+               }
+               // 不满足则手机号不正确
+                else {
+                   log.info("手机号和验证码不匹配，请重试");
+                   resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"手机号和验证码不匹配，请重试！");
+               }
             }else {
+                log.info("验证码不正确，请重新输入");
                 resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"验证码不正确，请重新输入");
             }
 
@@ -129,21 +143,71 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         else {
             LambdaQueryWrapper<User> qw = new LambdaQueryWrapper<>();
             qw.eq(User::getUserOpenid,openid);
+            // 微信号对应账户
             User existedUser = userMapper.selectOne(qw);
+
+            LambdaQueryWrapper<User> qw1 = new LambdaQueryWrapper<>();
+            // 查询是否存在tel 一个tel对应一个用户
+            // 微信一个用户 手机一个用户 在手机号相同的情况下
+            // 所以添加后面条件来过滤微信
+            qw1.eq(User::getUserTel,tel).eq(User::getUserOpenid,null);
+            // 手机号对应账户
+            User one = userMapper.selectOne(qw1);
             // 判断验证码
             if (captcha.equalsIgnoreCase(telCaptcha)){
-                // 判断微信用户之前是否绑定过此手机号
-                if (ObjectUtils.isEmpty(existedUser.getUserTel()) || !existedUser.getUserTel().equals(tel) ){
-                    existedUser.setUserTel(tel);
-                    // 更新用户
-                    int i = userMapper.updateUserById(existedUser);
-                    log.info("更新用户:{}",existedUser);
+                // 同时验证码与手机号对应才满足条件
+                if (tel.equals(redisUtils.get(telCaptcha))){
+                    // 手机号不存在时可以插入
+//                    if (ObjectUtils.isEmpty(one)){
+
+                        // 判断此手机号之前是否存在 存在插入微信信息
+                        if (!ObjectUtils.isEmpty(one)){
+                            // 手机号已经存在 把微信信息更新进去
+                            one.setUserName(existedUser.getUserName());
+                            one.setUserHeadImg(existedUser.getUserHeadImg());
+                            one.setUserOpenid(existedUser.getUserOpenid());
+                            userMapper.updateById(one);
+                            // 把微信信息和手机信息整合 删除微信信息
+                            userMapper.deleteById(existedUser.getUserId());
+                            log.info("微信用户-更新用户:{}",one);
+                            resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,existedUser);
+                        }
+                        // 判断微信用户之前是否绑定过此手机号(其他手机号 手机号为空满足条件) ||
+                       else if (ObjectUtils.isEmpty(existedUser.getUserTel()) || !existedUser.getUserTel().equals(tel)){
+                            // 没有绑定此手机号
+                            existedUser.setUserTel(tel);
+                            // 更新用户
+                            int i = userMapper.updateUserById(existedUser);
+                            log.info("微信用户-更新用户:{}",existedUser);
+                            resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,existedUser);
+                        }
+//                    }
+//                    else {
+//                        // 手机号已经存在 把微信信息更新进去
+//                        one.setUserName(existedUser.getUserName());
+//                        one.setUserHeadImg(existedUser.getUserHeadImg());
+//                        one.setUserOpenid(existedUser.getUserOpenid());
+//                        userMapper.updateById(one);
+//                        // 把微信信息和手机信息整合 删除微信信息
+//                        userMapper.deleteById(existedUser.getUserId());
+//                        log.info("微信用户-更新用户:{}",one);
+//                    }
+
+                    log.info("微信用户-登录成功，无需更新");
+
                 }
-                log.info("登录成功，无需更新");
-                resultMap.put(CommonConstant.UNIFY_RETURN_SUCCESS_CODE,existedUser);
+                // 不满足则手机号不正确
+                else {
+                    log.info("手机号和验证码不匹配，请重试");
+                    resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"手机号和验证码不匹配，请重试！");
+                }
+
             }else {
+                log.info("微信用户-验证码不正确，请重新输入 ");
                 resultMap.put(CommonConstant.UNIFY_RETURN_FAIL_CODE,"验证码不正确，请重新输入");
             }
+            // 释放手机号和验证码对应
+            redisUtils.del(telCaptcha);
             // 释放openid
             redisUtils.del("openid");
         }
---
+++ bailian-web/src/main/java/com/cuning/config/MyWebMvcConfig.java
@@ -0,0 +1,124 @@
+package com.cuning.config;
+
+
+import com.cuning.interceptor.JWTInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author tengjiaozhai
+ * @Description TODO
+ * @createTime 2022年06月01日 11:09:00
+ */
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public JWTInterceptor requestPermissionInterceptor(){
+        return  new JWTInterceptor();
+    }
+
+
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer pathMatchConfigurer) {
+
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
+
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer asyncSupportConfigurer) {
+
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry formatterRegistry) {
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        // 手动添加自定义请求许可拦截器
+        interceptorRegistry.addInterceptor(requestPermissionInterceptor()).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> list) {
+        // 请求参数解析器
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> list) {
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
+
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+    }
+
+    @Override
+    public Validator getValidator() {
+        return null;
+    }
+
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        return null;
+    }
+}
