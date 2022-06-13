4c0d91080d31c9e98efdc440e5a40172ff155da5
dingjunkai <2240299631@qq.com>
2022-06-13 08:59:35 +0800

    暂存

---
+++ bailian-web/src/main/java/com/cuning/controller/order/OrderController.java
@@ -0,0 +1,18 @@
+package com.cuning.controller.order;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "订单模块")
+@RequestMapping("/order")
+public class OrderController {
+
+    /**
+     * 创建订单 需要传递：购物车信息，用户信息
+     */
+
+
+}
