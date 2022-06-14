d198fa33901c675108220cb3ab1c8e96c24124e4
hujunzhuo <hujunzhuo@example.com>
2022-06-14 18:47:37 +0800

    评论

--- bailian-order/src/main/java/com/cuning/mapper/GoodsCommentaryMapper.java
+++ bailian-order/src/main/java/com/cuning/mapper/GoodsCommentaryMapper.java
@@ -1,14 +1,14 @@
-package com.cuning.mapper;
-/*
- * @Created on : 2022/6/13 0013
- * <p>
- * @Author     : Administrator
- * <p>
- * @Description: GoodsCommentaryMapper
- **/
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cuning.bean.goods.BailianGoodsCommentary;
-
-public interface GoodsCommentaryMapper extends BaseMapper<BailianGoodsCommentary> {
-}
+//package com.cuning.mapper;
+///*
+// * @Created on : 2022/6/13 0013
+// * <p>
+// * @Author     : Administrator
+// * <p>
+// * @Description: GoodsCommentaryMapper
+// **/
+//
+//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+//import com.cuning.bean.goods.BailianGoodsCommentary;
+//
+//public interface GoodsCommentaryMapper extends BaseMapper<BailianGoodsCommentary> {
+//}
