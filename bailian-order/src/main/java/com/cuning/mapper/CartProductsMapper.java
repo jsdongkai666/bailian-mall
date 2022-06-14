a4d2d5d0d148cc84f61d480d12ff112dac4a32f5
hujunzhuo <hujunzhuo@example.com>
2022-06-14 19:32:11 +0800

    订单

--- bailian-order/src/main/java/com/cuning/mapper/CartProductsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cuning.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cuning.bean.shoppingcarts.BailianCartProducts;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CartProductsMapper extends BaseMapper<BailianCartProducts> {
-}
