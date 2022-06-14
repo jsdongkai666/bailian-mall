d9814e28fb0cbd6c591117bc452a0c8e76d2debd
lixu <13775075304@163.com>
2022-06-14 09:47:15 +0800

    。

--- bailian-comm/src/main/java/com/cuning/bean/shoppingOrder/BailianOrder.java
+++ bailian-comm/src/main/java/com/cuning/bean/shoppingOrder/BailianOrder.java
@@ -5,12 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.models.auth.In;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created On : 2022/6/11.
@@ -74,6 +72,7 @@ public class BailianOrder {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date updateTime;
 
-    @TableField(exist = false)
-    private List<BailianOrderItem> bailianOrders;
+
+//    @TableField(exist = false)
+//    private List<BailianOrderItem> bailianOrders;
 }
