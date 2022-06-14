d6136e3cbceec0ed561716b6316ccc31279e3b9f
wangdefeng <wangdefeng@zbitedu.cn>
2022-06-14 08:35:57 +0800

    11

---
+++ bailian-search/src/main/java/com/cuning/mapper/EsMapper.java
@@ -0,0 +1,16 @@
+package com.cuning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cuning.bean.BailianGoodsInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created On : 2022/6/12.
+ * <p>
+ * Author     : lenovo
+ * <p>
+ * Description: EsMapper
+ */
+@Mapper
+public interface EsMapper extends BaseMapper<BailianGoodsInfo> {
+}
