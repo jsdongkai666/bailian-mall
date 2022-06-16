1cc964a7d04ff52f0763f5003677d847969b7f05
hujunzhuo <hujunzhuo@example.com>
2022-06-16 14:48:06 +0800

    商品上下架状态改变

--- bailian-order/src/main/java/com/cuning/service/impl/GoodsCommentaryServiceImpl.java
+++ bailian-order/src/main/java/com/cuning/service/impl/GoodsCommentaryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.cuning.service.Impl;
+package com.cuning.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
